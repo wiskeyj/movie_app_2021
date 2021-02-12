e056de6c10813509dd73d5cca1a364fa14e19c39
wiskeyj <wxyzi1226@gmail.com>
2021-02-12 19:21:23 +0900

    04-4 prop-types

--- src/App.js
+++ src/App.js
@@ -1,37 +1,51 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Food({ name, picture }){
+function Food({ name, picture, rating }){
   return (
     <div>
       <h2>I Love {name}</h2>
-      <img src={picture}/>
+      <h4>☆{rating}/5.0</h4>
+      <img src={picture} alt={name}/>
     </div>
     );
 }
 
 const foodLike = [
   {
-    name : 'Mint',
+    id : 1,
+    name : '민초초',
     image : 'http://www.baskinrobbins.co.kr/upload/product/1530775682.png',
+    rating : 5.0,
   },
   {
-    name : 'Tteokbokki',
+    id : 2,
+    name : '떡볶이',
     image : 'https://media.istockphoto.com/photos/tteokbokki-with-eggs-in-gray-bowl-on-concrete-table-top-tteokbokki-is-picture-id1253629795?k=6&m=1253629795&s=612x612&w=0&h=gbHUJQQQa8KVRPEQi-98JvrLtx9wPF7GlkNIQD6hj9s=',
+    rating : 4.0,
   },
   {
-    name : 'Americano',
+    id : 3,
+    name : '얼죽아',
     image : 'http://image.auction.co.kr/itemimage/18/79/2f/18792f67a6.jpg',
+    rating : 4.5,
   },
 ]; 
 
 function App() { // App 컴포넌트 정의
   return (
     <div>
-      {foodLike.map(dish => (
-        <Food name={dish.name} picture={dish.image}/>
+      {foodLike.map(dish=> (
+        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
       ))}
     </div>
   ); // dish에 foodLike의 원소가 하나씩 넘어오면 그 값을 name props에 전달한다.
 }
+
+Food.propTypes = {
+  name : PropTypes.string.isRequired,
+  picture : PropTypes.string.isRequired,
+  rating : PropTypes.number,
+};
 
 export default App;
