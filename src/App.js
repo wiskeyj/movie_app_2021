7191b5cb8d35886ebc148ff836939dde2b9c880b
wiskeyj <wxyzi1226@gmail.com>
2021-02-12 16:43:49 +0900

    03-3 JSX

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Potato from './Potato';
 
 function App() { // App 컴포넌트 정의
   return (
     <div>
       <h1>Hello~!</h1> 
+      <Potato />
     </div>
   );// App 컴포넌트가 html을 반환
 }
---
+++ src/Potato.js
@@ -0,0 +1,8 @@
+import React from 'react'; // 이 코드를 입력해야 리액트가 JSX를 이해할 수 있다.
+// JSX ; javascript + HTML을 조합한 문법
+
+function Potato(){
+    return <h3>I Hate Potato^^</h3>
+}
+
+export default Potato; // 다른 파일에서 Potato 컴포넌트를 사용할 수 있다. 
