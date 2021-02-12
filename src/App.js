a2d4124643808f2aea71ba70afe9689ec98e8448
wiskeyj <wxyzi1226@gmail.com>
2021-02-12 16:34:54 +0900

    03-2 Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-function App() {
-  return <div>Hello!</div>
+function App() { // App 컴포넌트 정의
+  return (
+    <div>
+      <h1>Hello~!</h1> 
+    </div>
+  );// App 컴포넌트가 html을 반환
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App'; // App 컴포넌트를 임포트하여 사용
 
-// App 컴포턴트를 아이디가 root인 엘리먼트에 그린다.
 ReactDOM.render(<App /> ,document.getElementById('root'));
+
+// <App />은 HTML 태그가 아니다.
+// <App />을 ReactDOM.render()함수의 첫 번째 인자로 전달하면 App 컴포넌트가 반환하는 것들을 화면에 그릴 수 있다.
+// <App />을 ReactDOM.render()함수의 두 번째 인자에 App 컴포넌트가  그려질 위치를 전달한다.
+// -> App 컴포넌트는 아이디가 root인 엘리먼트에 그려질 것 이다.
