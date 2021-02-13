02a73e19ec9a70b8230655c82cb8b2f35726650a
wiskeyj <wxyzi1226@gmail.com>
2021-02-13 21:35:18 +0900

    06-2 getMovies

--- src/App.js
+++ src/App.js
@@ -8,8 +8,13 @@ class App extends React.Component{
     };
 
     getMovies = async () =>{ // async : getMovies()가 비동기 함수 - 시간이 필요함을 javascript에 알림
-        const movies = await axios.get('https://yts-proxy.now.sh/list_movies.json');
         //await : axios.get()의 실행을 기다리라 지시
+        const {
+            data :{
+                data : {movies},
+            },
+        } = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+        this.setState({movies, isLoading : false});
     }
 
     componentDidMount(){
